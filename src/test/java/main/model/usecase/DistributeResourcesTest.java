54919e0bb779d61c3a0a32aba12a42e91e7d647d
huangken8511429 <huangken8511429@gmail.com>
2022-12-05 16:22:24 +0800

    Refactor : DistributeResourcesTest add setUP function

--- src/test/java/main/model/usecase/DistributeResourcesTest.java
+++ src/test/java/main/model/usecase/DistributeResourcesTest.java
@@ -1,6 +1,7 @@
 package main.model.usecase;
 
 import main.model.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,6 +10,21 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DistributeResourcesTest {
+    private Player playerA;
+    private Player playerB;
+    private Establishment wheatField;
+    private Establishment cafe;
+    private Game game;
+
+    @BeforeEach
+    void setUP() {
+        playerA = new Player("A");
+        playerB = new Player("B");
+        wheatField = new WheatField();
+        cafe = new Cafe();
+        game = new Game(new Bank(100), List.of(playerA, playerB), List.of(new Dice()), new Marketplace());
+    }
+
     @Test
     @DisplayName(
             "given 有銀行(100 coin)、A 玩家(0 coin)及 B 玩家，A 玩家手牌裡有小麥田 " +
@@ -16,18 +32,11 @@ public class DistributeResourcesTest {
                     "then A 玩家從銀行獲得1元(銀行 coin = 99, A 玩家 coin = 1)")
     void testWheatField() {
         // given
-        Player playerA = new Player("A");
-        Establishment wheatField = new WheatField();
         playerA.addCardToHandCard(wheatField);
 
-        Player playerB = new Player("B");
-
-        Game game = new Game(new Bank(100), List.of(playerA, playerB), List.of(new Dice()), new Marketplace());
-
         // when
-        int playerBDicePoint = 1;
         game.setTurnPlayer(playerA);
-        game.distributeResources(playerBDicePoint);
+        settingDicePointAndTakeEffect(1, game);
 
         // then
         assertEquals(99, game.getBank().getTotalCoin());
@@ -40,16 +49,12 @@ public class DistributeResourcesTest {
                     "when B 玩家擲骰子是3時，系統分配資源後 " +
                     "then A 玩家從玩家B獲得1元(B玩家 coin = 4, A 玩家 coin = 2)")
     void playerB_has_OneCafe() {
-        Establishment cafe = new Cafe();
-        Player playerA = new Player("A");
-        Player playerB = new Player("B");
+        //given
         playerB.addCardToHandCard(cafe);
 
-        Game game = new Game(new Bank(100), List.of(playerA, playerB), List.of(new Dice()), new Marketplace());
         // when
-        int playerBDicePoint = 3;
         game.setTurnPlayer(playerA);
-        game.distributeResources(playerBDicePoint);
+        settingDicePointAndTakeEffect(3, game);
 
         // then
         assertEquals(100, game.getBank().getTotalCoin());
@@ -63,17 +68,12 @@ public class DistributeResourcesTest {
                     "when A 玩家擲骰子是3時，系統分配資源後 " +
                     "then B 玩家從玩家A獲得3元(B玩家 coin = 6, A 玩家 coin = 0)")
     void playerB_has_threeCafe() {
-        //given
-        Establishment cafe = new Cafe();
-        Player playerA = new Player("A");
+        // given
+        setPlayer_has_threeCafes(playerB);
 
-        Player playerB = setPlayerB_has_threeCafes(cafe);
-
-        Game game = new Game(new Bank(100), List.of(playerA, playerB), List.of(new Dice()), new Marketplace());
         // when
-        int playerBDicePoint = 3;
         game.setTurnPlayer(playerA);
-        game.distributeResources(playerBDicePoint);
+        settingDicePointAndTakeEffect(3, game);
 
         // then
         assertEquals(100, game.getBank().getTotalCoin());
@@ -87,18 +87,13 @@ public class DistributeResourcesTest {
                     "when A 玩家擲骰子是3時，系統分配資源後 " +
                     "then B 玩家從玩家A獲得2元(B玩家 coin = 5, A 玩家 coin = 0)")
     void playerB_has_threeCafe_but_player_money_isNot_enough() {
-        //given
-        Establishment cafe = new Cafe();
-        Player playerA = new Player("A");
+        // given
         playerA.payCoin(1);
+        setPlayer_has_threeCafes(playerB);
 
-        Player playerB = setPlayerB_has_threeCafes(cafe);
-
-        Game game = new Game(new Bank(100), List.of(playerA, playerB), List.of(new Dice()), new Marketplace());
         // when
-        int playerBDicePoint = 3;
         game.setTurnPlayer(playerA);
-        game.distributeResources(playerBDicePoint);
+        settingDicePointAndTakeEffect(3, game);
 
         // then
         assertEquals(100, game.getBank().getTotalCoin());
@@ -106,11 +101,14 @@ public class DistributeResourcesTest {
         assertEquals(5, playerB.getTotalCoin());
     }
 
-    private static Player setPlayerB_has_threeCafes(Establishment cafe) {
-        Player playerB = new Player("B");
-        playerB.addCardToHandCard(cafe);
-        playerB.addCardToHandCard(cafe);
-        playerB.addCardToHandCard(cafe);
-        return playerB;
+    private static void settingDicePointAndTakeEffect(int point, Game game) {
+        game.setCurrentDicePoint(point);
+        game.distributeResources(game.getCurrentDicePoint());
+    }
+
+    private void setPlayer_has_threeCafes(Player player) {
+        player.addCardToHandCard(cafe);
+        player.addCardToHandCard(cafe);
+        player.addCardToHandCard(cafe);
     }
 }
