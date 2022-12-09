6b9ba86342414ec76e3257570410a6098219ae02
yicyuanliang <gnr1118@gmail.com>
2022-12-09 22:09:34 +0800

    [implement] implement ranch

--- src/main/java/main/model/Ranch.java
+++ src/main/java/main/model/Ranch.java
@@ -1,7 +1,15 @@
 package main.model;
 
-public class Ranch {
-    public void takeEffect(Game game, Player owner) {
+public class Ranch extends Establishment {
 
+    public Ranch() {
+        super("牧場", 1, CardType.ANIMAL_HUSBANDRY, 6, 2, Industry.BLUE);
+    }
+
+    public void takeEffect(Game game, Player owner) {
+        if (isDicePointToTakeEffect(game.getCurrentDicePoint())) {
+            game.getBank().payCoin(1);
+            owner.gainCoin(1);
+        }
     }
 }
