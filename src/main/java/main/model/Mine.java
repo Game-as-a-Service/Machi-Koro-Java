01cb560a0c6cfea251915d68c1d4904a318d51c6
akechetaki10736 <chi972121@gmail.com>
2022-12-10 20:24:44 +0800

    style(Add description of effect.):

--- src/main/java/main/model/Mine.java
+++ src/main/java/main/model/Mine.java
@@ -7,6 +7,7 @@ public class Mine extends Establishment {
 
     @Override
     public void takeEffect(Game game, Player player) {
+        // 當骰子點數為 9，持有該卡片的玩家從銀行獲得 5 元
         if (isDicePointToTakeEffect(game.getCurrentDicePoint())) {
             game.getBank().payCoin(5);
             player.gainCoin(5);
