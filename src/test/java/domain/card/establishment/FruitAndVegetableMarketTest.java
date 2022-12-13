9ef28ff7585429ef91e19e83613e246845fc959a
Ken <keytsai513@gmail.com>
2022-12-14 00:03:02 +0800

    Add FruitAndVegetableMarket

---
+++ src/test/java/domain/card/establishment/FruitAndVegetableMarketTest.java
@@ -0,0 +1,38 @@
+package domain.card.establishment;
+
+import domain.Game;
+import domain.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import utils.TestGameBuilder;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class FruitAndVegetableMarketTest {
+
+    private Game game;
+    private final FruitAndVegetableMarket fruitAndVegetableMarket = new FruitAndVegetableMarket();
+    private Player player;
+
+    @BeforeEach
+    void setUp() {
+        game = new TestGameBuilder().setFixedDicePoint(11).setPlayers(1).build();
+        player = game.getPlayers().get(0);
+    }
+
+    @Test
+    void testTakeEffect() {
+        WheatField wheatField = new WheatField();
+        AppleOrchard appleOrchard = new AppleOrchard();
+        player.addCardToHandCard(wheatField);
+        player.addCardToHandCard(appleOrchard);
+        int originalPlayerTotalCoin = player.getTotalCoin();
+        int originalBankTotalCoin = game.getBank().getTotalCoin();
+
+        game.setTurnPlayer(player);
+        fruitAndVegetableMarket.takeEffect(game, player);
+
+        assertThat(player.getTotalCoin()).isEqualTo(originalPlayerTotalCoin + 4);
+        assertThat(game.getBank().getTotalCoin()).isEqualTo(originalBankTotalCoin - 4);
+    }
+}
