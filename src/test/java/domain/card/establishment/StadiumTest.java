213dbe15cf0170944f73cf3113af08f52700c706
akechetaki10736 <chi972121@gmail.com>
2022-12-19 10:45:22 +0800

    test(StadiumTest): Add missing test scenario.
    New test case: when player don't have enough coin, they will only pay
    the rest of coin.

--- src/test/java/domain/card/establishment/StadiumTest.java
+++ src/test/java/domain/card/establishment/StadiumTest.java
@@ -17,6 +17,7 @@ class StadiumTest {
     private Player player3;
     private Player player4;
 
+
     @BeforeEach
     void setUp() {
         stadium = new Stadium();
@@ -25,8 +26,6 @@ class StadiumTest {
         player2 = game.getPlayers().get(1);
         player3 = game.getPlayers().get(2);
         player4 = game.getPlayers().get(3);
-
-
     }
 
     @Test
@@ -37,9 +36,8 @@ class StadiumTest {
         var originalBalanceOfPlayer3 = player3.getTotalCoin();
         var originalBalanceOfPlayer4 = player4.getTotalCoin();
 
-
         game.setTurnPlayer(player1);
-        stadium.takeEffect(game, player1);
+        stadium.takeEffect(game);
 
         var balanceOfPlayer1 = player1.getTotalCoin();
         var balanceOfPlayer2 = player2.getTotalCoin();
@@ -50,5 +48,32 @@ class StadiumTest {
         assertThat(balanceOfPlayer2).isEqualTo(originalBalanceOfPlayer2 - 2);
         assertThat(balanceOfPlayer3).isEqualTo(originalBalanceOfPlayer3 - 2);
         assertThat(balanceOfPlayer4).isEqualTo(originalBalanceOfPlayer4 - 2);
+    }
+
+    @Test
+    void takeEffectWhenPlayerDontHaveEnoughMoney() {
+        player3.payCoin(2); // left 1 coin
+        player4.payCoin(3); // left 0 coin
+
+        player1.addCardToHandCard(stadium);
+        var originalBalanceOfPlayer1 = player1.getTotalCoin();
+        var originalBalanceOfPlayer2 = player2.getTotalCoin();
+        var originalBalanceOfPlayer3 = player3.getTotalCoin();
+        var originalBalanceOfPlayer4 = player4.getTotalCoin();
+
+        game.setTurnPlayer(player1);
+        stadium.takeEffect(game);
+
+        var balanceOfPlayer1 = player1.getTotalCoin();
+        var balanceOfPlayer2 = player2.getTotalCoin();
+        var balanceOfPlayer3 = player3.getTotalCoin();
+        var balanceOfPlayer4 = player4.getTotalCoin();
+
+        assertThat(balanceOfPlayer1).isEqualTo(originalBalanceOfPlayer1 + 3);
+        assertThat(balanceOfPlayer2).isEqualTo(originalBalanceOfPlayer2 - 2);
+        assertThat(balanceOfPlayer3).isEqualTo(originalBalanceOfPlayer3 - 1);
+        assertThat(balanceOfPlayer4).isEqualTo(originalBalanceOfPlayer4);
+
+
     }
 }
