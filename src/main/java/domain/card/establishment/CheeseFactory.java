aed6940d007c63e4e40c0cd7ad4a0c8249a42b5f
Chanwu77 <a1687336@gmail.com>
2023-01-15 22:36:59 +0800

    fix cheesefactory typo

--- src/main/java/domain/card/establishment/CheeseFactory.java
+++ src/main/java/domain/card/establishment/CheeseFactory.java
@@ -14,14 +14,14 @@ public class CheeseFactory extends Establishment {
     }
 
     protected void doTakeEffect(Game game) {
-        // 當你自己骰出這個數字時，每擁有一張[牧場]符號的建築，就可以從銀行獲得3元。
+        // 當你自己骰出這個數字時，每擁有一張[CardType.ANIMAL_HUSBANDRY]符號的建築，就可以從銀行獲得3元。
         if (game.isTurnPlayer(getOwner())) {
-            int animalHusbandaryCardsCount =
+            int animalHusbandryCardsCount =
                     (int) getOwner().getOwnedEstablishment().stream().filter(establishment -> establishment.getCardType().equals(ANIMAL_HUSBANDRY)).count();
             //如果玩家自己沒有擁有牧場卡牌，就得不到任何收入?
-            if (animalHusbandaryCardsCount > 0) {
-                game.getBank().payCoin(animalHusbandaryCardsCount * 3);
-                getOwner().gainCoin(animalHusbandaryCardsCount * 3);
+            if (animalHusbandryCardsCount > 0) {
+                game.getBank().payCoin(animalHusbandryCardsCount * 3);
+                getOwner().gainCoin(animalHusbandryCardsCount * 3);
             }
         }
     }
