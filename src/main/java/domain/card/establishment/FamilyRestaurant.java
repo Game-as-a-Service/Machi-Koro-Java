57256276f0aeb2e99dc385a47ba78a865b80f3ac
Chanwu77 <a1687336@gmail.com>
2023-05-04 21:33:07 +0800

    fix paycoin method conflict

--- src/main/java/domain/card/establishment/FamilyRestaurant.java
+++ src/main/java/domain/card/establishment/FamilyRestaurant.java
@@ -8,7 +8,7 @@ import java.util.Set;
 public class FamilyRestaurant extends Establishment{
 
     private final int EFFECT_COIN = 2;
-    public FamilyRestaurant() { super("家庭餐廳",3, CardType.RESTAURANT, Set.of(9,10) , Industry.RED); }
+    public FamilyRestaurant() { super("家庭餐廳",3, CardType.RESTAURANT, Set.of(9,10) , IndustryColor.RED); }
 
     @Override
     protected void doTakeEffect(Game game) {
